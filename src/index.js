51c85dcd32b237e32dc8c190357220a5986fa2ba
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-09-23 21:45:23 +0900

    finished navbar component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./components/navbar.css"
+import "./app.css"
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
