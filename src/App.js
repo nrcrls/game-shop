b26a4c8695dcf92f6f7cd682dc4b2ea9e5465541
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-09-24 18:45:38 +0900

    created routes and installed axios

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import Navbar from "./components/Navbar";
 import Home from "./routes/Home";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from 'axios';
 
 const App = () => {
+  const getGame = () => {
+    axios.get(`https://api.rawg.io/api/platforms?key=${process.env.REACT_APP_PRIVATE_KEY}`).then(
+      (response) => {
+        console.log(response);
+      });
+  }
+
   return (
     <Router>
       <div>
         <Navbar />
         <div className="content">
           <Routes>
-            <Route exact path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </div>
         <Footer />
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+const Banner = () => {
+  return (
+    <div className='hero'>
+
+    </div>
+  );
+}
+
+export default Banner
